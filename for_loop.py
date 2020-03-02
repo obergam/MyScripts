5a71b09689d9940440a154c459a1687c368f61b0
obergam <ouaddahbergam@gmail.com>
2020-03-01 19:49:02 -0700

    more scripts

--- for_loop.py
+++ for_loop.py
@@ -27,12 +27,53 @@
 #
 # greet_friends(['Taylor', 'Louis', 'Jay', 'Adam'])
 # greet_friends('Barry')
+#
+# def factorial(n):
+# #   result = 1
+# #   for x in range(n):
+# #     result = result * n
+# #   return result
+# #
+# #
+# # for n in range(10):
+# #   print(n, factorial(n))
 
-def validate_users(users):
-  for user in users:
-    if is_valid(user):
-      print(user + " is valid")
-    else:
-      print(user + " is invalid")
 
-validate_users("purplecat")
+# def validate_users(users):
+#   for user in users:
+#     if is_valid(user):
+#       print(user + " is valid")
+#     else:
+#       print(user + " is invalid")
+#
+# validate_users("purplecat")
+#
+# def multiple(n):
+#   for n in range(101):
+#     if 7 * n < 100:
+#       result = 7 * n
+#       print(result)
+#
+#
+# multiple(100)
+# def retry(operation, attempts):
+#   for n in range(attempts):
+#     if operation():
+#       print("Attempt " + str(n) + " succeeded")
+#       break
+#     else:
+#       print("Attempt " + str(n) + " failed")
+#
+# retry(create_user, 3)
+# retry(stop_service, 5)
+
+def factorial(n):
+  result = 1
+  for x in range(n):
+    result = n * result
+    n = n - 1
+  return result
+
+
+for n in range(10):
+  print(n, factorial(n))
--- testStuff.py
+++ testStuff.py
@@ -13,15 +13,31 @@ print(is_power_of_two(1))  # Should be True
 print(is_power_of_two(8))  # Should be True
 print(is_power_of_two(9))  # Should be False
 """
-def sum_divisors(n):
-  # Return the sum of all divisors of n, not including n
-    quotient = 1
-    result = 0
-    while quotient < n:
-        if n % quotient == 0:
-            result = result + quotient
-        quotient += 1
-    return result
+# def sum_divisors(n):
+#   # Return the sum of all divisors of n, not including n
+#     quotient = 1
+#     result = 0
+#     while quotient < n:
+#         if n % quotient == 0:
+#             result = result + quotient
+#         quotient += 1
+#     return result
+#
+# print(sum_divisors(6)) # Should be 1+2+3=6
+# print(sum_divisors(12)) # Should be 1+2+3+4+6=16
+#
+# def factorial(n):
+#     result = 1
+#     for x in range(n):
+#         result = result * n
+#     return result
+#
+# for n in range(10):
+#     print(n, factorial(n))
 
-print(sum_divisors(6)) # Should be 1+2+3=6
-print(sum_divisors(12)) # Should be 1+2+3+4+6=16
+def cube(n):
+    for x in range(1,n+1):
+        result = x **3
+        print(result)
+
+cube(10)
