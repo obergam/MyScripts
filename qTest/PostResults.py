794596d5b603727fd217c2a294dc75c27a889c78
Ouaddah Shuttle <ouaddahbergam@gmail.com>
2020-09-15 16:33:06 -0600

    adding qTest script and sample

---
+++ qTest/PostResults.py
@@ -0,0 +1,34 @@
+import requests
+
+qTestTriggerURL = "https://pulse-7.qtestnet.com/webhook/b0e5d8e7-4a75-47fc-a392-1e2580585967"
+qtest_projectID = 89593
+qtest_testcycle = 2024376
+
+
+def myPoster(data):
+	sendSuccessful = False
+	try:
+		response = requests.post(qTestTriggerURL, json=data)
+		sendSuccessful = True
+	except Exception as e:
+		print('requests.post had an exception: {}'.format(e))
+
+	if sendSuccessful == True:
+		print('qTest reporting succeeded')
+		pass
+	else:
+		print('qTest reporting failed')
+
+
+filepath = ('Sample.xml')
+data = {}
+
+with open(filepath, 'r') as file:
+	resultsXml = file.read().replace('"', '\"')
+
+data['projectId'] = qtest_projectID
+data['testCycle'] = qtest_testcycle
+data['result'] = resultsXml
+
+myPoster(data)
+
